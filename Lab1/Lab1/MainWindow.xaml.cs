4defc4646647200b40af66835e9a71d0ee52b018
IdkWhatToWriteHereOhManItWasAlreadyUsed <dimoon.amid05@gmail.com>
2025-09-04 23:02:34 +0300

    Чтение файла и таблица.Редизайн.

--- Lab1/Lab1/MainWindow.xaml.cs
+++ Lab1/Lab1/MainWindow.xaml.cs
@@ -1,172 +1,58 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 
 namespace Lab1;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
 public partial class MainWindow : Window
 {
+    string typescriptCode = @"";
     public MainWindow()
     {
         InitializeComponent();
     }
 
+    private void OpenFileClick(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog();
+        if ((bool)dialog.ShowDialog())
+        {
+            typescriptCode = File.ReadAllText(dialog.FileName);
+            tbCode.Text = typescriptCode;
+        }
+    }
+
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        var halstead = new Halstead();
-        string typescriptCode = @"
-            const gridSize = 9;
+        var halstead = new Halstead();     
+        var data = halstead.ParseTypeScript(typescriptCode);
+        var items = new List<TokenItem>();
+        var opKeys = data.Item1.Keys.ToList();
+        var opdKeys = data.Item2.Keys.ToList();
+        int maxRows = Math.Max(opKeys.Count, opdKeys.Count);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const solveButton = document.getElementById('solve-btn');
-    solveButton.addEventListener('click', solveSudoku);
-
-    const clearButton = document.getElementById('clear-btn');
-    clearButton.addEventListener('click', clearGrid);
-
-    const sudokuGrid = document.getElementById('sudoku-grid');
-
-    for (let row = 0; row < gridSize; row += 1) {
-        const newRow = document.createElement('tr');
-        for (let col = 0; col < gridSize; col += 1) {
-            const cell = document.createElement('td');
-            const input = document.createElement('input');
-            input.type = 'number';
-            input.className = 'cell';
-            input.id = `cell-${row}-${col}`;
-            cell.appendChild(input);
-            newRow.appendChild(cell);
-        }
-        sudokuGrid?.appendChild(newRow);
-    }
-});
-
-async function solveSudoku() {
-    const clearButton = document.getElementById('clear-btn') as HTMLButtonElement;
-    clearButton.disabled = true;
-    const sudokuArray : number[][] = [];
-
-    for (let row = 0; row < gridSize; row += 1) {
-        sudokuArray[row] = [];
-        for (let col = 0; col < gridSize; col += 1) {
-            const cellId = `cell-${row}-${col}`;
-            const cell = document.getElementById(cellId) as HTMLInputElement;
-            const cellValue = cell.value;
-            sudokuArray[row][col] = cellValue !== '' ? parseInt(cellValue) : 0;
-
-            if (sudokuArray[row][col] !== 0) {
-                cell.classList.add('user-input');
-            }
-        }
-    }
-
-    if (solveSudokuHelper(sudokuArray, 0, 0)) {
-        alert('Solution exists for the given Sudoku.');
-    } else {
-        alert('No solution exists for the given Sudoku.');
-    }
-    clearButton.disabled = false;
-}
-
-function solveSudokuHelper(board : number[][], row : number, col : number) : boolean {
-    if (col === 9) {
-        col = 0;
-        row += 1;
-        if (row === 9) {
-            return true;
-        }
-    }
-
-    if (board[row][col] !== 0) {
-        return solveSudokuHelper(board, row, col + 1);
-    }
-
-    for (let c = 1; c <= 9; c += 1) {
-        if (isValid(board, row, col, c)) {
-            board[row][col] = c;
-
-            if (solveSudokuHelper(board, row, col + 1)) {
-                return true;
-            }
-
-            board[row][col] = 0;
-        }
-    }
-
-    return false;
-
-    function isValid(board : number[][], row : number, col : number, c : number) : boolean {
-        
-        for (let i = 0; i < 9; i += 1) {
-            if (board[row][i] === c) {
-                return false;
-            }
-            if (board[i][col] === c) {
-                return false;
-            }
-        }
-
-        let startRow = Math.floor(row / 3) * 3;
-        let startCol = Math.floor(col / 3) * 3;
-        for (let i = startRow; i < startRow + 3; i += 1) {
-            for (let j = startCol; j < startCol + 3; j += 1) {
-                if (board[i][j] === c) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-}
-
-function clearGrid() {
-    for (let row = 0; row < gridSize; row += 1) {
-        for (let col = 0; col < gridSize; col += 1) {
-            const cellId = `cell-${row}-${col}`;
-            const cell = document.getElementById(cellId) as HTMLInputElement;
-            cell.value = '';
-            cell.classList.remove('user-input', 'solved');
-        }
-    }
-}
-
-        ";
-
-        var result = halstead.ParseTypeScript(typescriptCode);
-
-        string text = "";
-
-        text += "Operators:\n";
-
-        foreach (var op in result.Item1)
+        for (int i = 0; i < maxRows; i++)
         {
-            text += $"  {op.Key}: {op.Value}\n";
+            items.Add(new TokenItem
+            {
+                Operator = i < opKeys.Count ? opKeys[i] : "",
+                OperatorCount = i < opKeys.Count ? data.Item1[opKeys[i]].ToString() : "",
+                Operand = i < opdKeys.Count ? opdKeys[i] : "",
+                OperandCount = i < opdKeys.Count ? data.Item2[opdKeys[i]].ToString() : ""
+            });
         }
 
-        text += "\nOperands:\n";
-        foreach (var op in result.Item2)
-        {
-            text += $"  {op.Key}: {op.Value}\n";
-        }
+        dataGrid.ItemsSource = items;
 
-        // Получение метрик
+        string metricsText = "";
         var metrics = halstead.GetMetrics(typescriptCode);
-        text += $"\nVolume: {metrics.Volume:F2}\n";
-        text += $"Difficulty: {metrics.Difficulty:F2}\n";
-        text += $"Effort: {metrics.Effort:F2}\n";
-
-        tb.Text = text;
+        metricsText += $"\n  Volume: {metrics.Volume:F2}\n";
+        // metricsText += $"Difficulty: {metrics.Difficulty:F2}\n";
+        // metricsText += $"Effort: {metrics.Effort:F2}\n";
+        tbMetrics.Text = metricsText;
     }
 }
---
+++ Lab1/Lab1/TokenItem.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Lab1
+{
+    public class TokenItem
+    {
+        public string Operator { get; set; }
+        public string OperatorCount { get; set; }
+        public string Operand { get; set; }
+        public string OperandCount { get; set; }
+    }
+}
